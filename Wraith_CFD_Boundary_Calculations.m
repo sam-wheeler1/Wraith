09c602aef281759225bc5f396adab1a23dc9ad8b
Sam Wheeler <samwheeler557@gmail.com>
2026-06-20 03:09:50 -0500

    Update Wraith_CFD_Boundary_Calculations.m
    Added Part 2

--- Wraith_CFD_Boundary_Calculations.m
+++ Wraith_CFD_Boundary_Calculations.m
@@ -2,50 +2,79 @@ clear
 clc
 close all
 
-%---------------------
-% Pressure Calculations
-%---------------------
-
 % This script is designed to calculate the inlet and outlet pressure
-% that will be applied in CFD on the inlet.
+% that will be applied in CFD on the inlet and thrust tube system
 
 %---------------------
-% Inlet Geometry Study
+% Part 1: Inlet Geometry Study
 %---------------------
 
-% The first step of our CFD process is creating a Latin Hypercube model
-% of the inlet with parameterized dimensions on inlet face, length,
-% curvature, and other design points
-
-
-%--------------
 % Parameters
-%--------------
-
-temperature_F = 70; %Farenenheit; we are assuming room temperature
-temperature_C = (temperature_F-32)*(5/9); %Celsius
-mi_per_hr = 300;
-m_per_s = mi_per_hr/2.237;
-speed_of_sound = 331 * sqrt(1 + (temperature_C/273.15));
-Freestream_Mach = m_per_s/speed_of_sound; % unitless
-Altitude = [0, 300, 10000]; % m; three altitudes: testing, rc flight, intended operation
+altitude = [0, 300, 10000]; % m; three altitudes: testing, rc flight, intended operation
+temperature_F = 70; % Farenheit; assume room temperature at 0 m altutitude
+temperature_C = (temperature_F-32)*(5/9) - 6.5*(altitude./1000); % Celsius
+temperature_K = temperature_C + 273.15; % Kelvin
+mi_per_hr = 300; % mi/hr
+m_per_s = mi_per_hr/2.237; %m/s
+speed_of_sound = 331 * sqrt(1 + (temperature_C/273.15)); %m/s
+freestream_Mach = m_per_s./speed_of_sound; % unitless
 specific_heat_ratio = 1.4; % assuming dry air
 
-%---------------------
+
 % Equations
-%---------------------
 
-% Static pressure does not include edf fan suction, which is not needed for
-% geometric comparion. This will be included later, however.
-static_pressure = 101325*(1 - 0.0065*Altitude/288.15).^5.2559; % not including change in pressure due to suction of edf, since part 1 is purely comparing geometry
+% Static pressure does not include edf fan suction, which is not needed for geometric comparion
+static_pressure = 101325*(1 - 0.0065*altitude/288.15).^5.2559;
 
-% Since the aircraft will be moving at subsonic speeds, we assume isentropic
-% flow and use the following equation to calculate the total pressure at the
-% inlet face
+% Since subsonic speeds, we assume isentropic flow and use the following equation to calculate
+% total pressure at the inlet face. By using Pressure Far-Field in ANSYS, 
+% total pressure will be auto calculated, however for research purposes the values are found manually
 
-total_pressure = static_pressure.*(1 + ((specific_heat_ratio - 1)/2)*Freestream_Mach.^2)^(specific_heat_ratio/(specific_heat_ratio-1));
+total_pressure = static_pressure.*(1 + ((specific_heat_ratio - 1)/2)*freestream_Mach.^2).^(specific_heat_ratio/(specific_heat_ratio-1));
 
-fprintf("The total pressure at the testing altitude is %.1f pa and the static pressure is %.1f pa \n", total_pressure(1),static_pressure(1))
-fprintf("The total pressure at the RC flight altitude is %.1f pa and the static pressure is %.1f pa \n", total_pressure(2),static_pressure(2))
-fprintf("The total pressure at the intended operation flight altitude is %.1f pa and the static pressure is %.1f pa \n", total_pressure(3),static_pressure(3))
+%---------------------------------------
+% Step 2: Finding pressure drop from EDF
+%---------------------------------------
 
+% Using the data given by the manufacturer to find mass flow rate of the EDF
+
+% Manufacturer Information
+
+% Note: Thrust assumed to be found in a static test, future work will
+% feature true tested fan performance
+
+thrust = (3790/1000)*9.81; % N; at full throttle
+radius_outer = 45.1/1000;
+radius_inner = 17.65/1000;
+
+% Parameters
+R_air = 287; % J/(kg*K)
+rho = static_pressure./(R_air * temperature_K);
+fan_swept_area = pi*(radius_outer^2 - radius_inner^2);
+
+% Mass flow rate at edf face found using thrust momentum equation
+% T = mdot(v_exit - v_in)
+v_static = 0;
+v_exit = sqrt((2*thrust)./(rho.*fan_swept_area) + v_static.^2);
+mdot_edf = rho.*fan_swept_area.*((v_exit + v_static)./2);
+
+%----------------
+%Print Statements
+%----------------
+
+for i = 1:length(altitude)
+    fprintf("----- Altitude: %.0f m -----\n", altitude(i))
+    
+    fprintf("Inlet Boundary: Pressure Far-Field\n")
+    fprintf("  Static Pressure: %.1f Pa\n", static_pressure(i))
+    fprintf("  Static Temperature: %.2f K\n", temperature_K(i))
+    fprintf("  Freestream Mach Number: %.3f\n", freestream_Mach(i))
+    fprintf("  Freestream Total Pressure: %.1f Pa\n\n", total_pressure(i))
+    
+    fprintf("EDF Boundary: Mass Flow at Fan Faces\n")
+    fprintf("  Mass Flow Rate In: %.3f kg/s\n", mdot_edf(i))
+    fprintf("  Mass Flow Rate Out: %.3f kg/s\n\n", mdot_edf(i))
+    
+    fprintf("Thrust Tube Exit Boundary: Pressure Outlet\n")
+    fprintf("  Static Pressure: %.1f Pa\n\n\n", static_pressure(i))
+end
